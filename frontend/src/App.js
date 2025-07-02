e736f1bd1d01ae650c88c7a8e3523bfa723a6d15
yasser <yasserfakir3@gmail.com>
2025-07-02 09:46:35 +0100

    all changes

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import Portfolio from './components/Portfolio';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
